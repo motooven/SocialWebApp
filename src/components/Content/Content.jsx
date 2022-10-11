77e68b4fea6b157e5f273b21abfc70a12bcc77a6
motooven <motooven@gmail.com>
2022-10-11 21:17:04 +0300

    'v15'

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -4,7 +4,14 @@ import s from './Content.module.css'
 
 const Content = () => {
     return <div className={s.content}>
-        Content
+        <img src="https://st3.depositphotos.com/1679906/17352/v/1600/depositphotos_173522714-stock-illustration-skull-in-gas-mask-illustration.jpg" alt="img" />
+        <div>
+            <div>ava + description</div>
+            <div>My post</div>
+            <div>New post</div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
     </div>
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,11 @@ import s from './Navbar.module.css'
 const Navbar = () => {
     return (
         <div className={s.navbar}>
-            Navbar
+            <div><a>Content</a></div>
+            <div><a>Messages</a></div>
+            <div><a>News</a></div>
+            <div><a>Music</a></div>
+            <div><a>Settings</a></div>
         </div>
     )
 }
