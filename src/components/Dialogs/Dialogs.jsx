df608e189ad2338876563b6808a0e6fbe2a1e76c
motooven <motooven@gmail.com>
2022-10-17 22:58:27 +0300

    v21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import s from './Dialogs.module.css'
 
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div>
+                <div>Oleg</div>
+                <div>Kaban</div>
+                <div>Vana</div>
+                <div>Jeka</div>
+                <div>Viktor</div>
+                <div>Mihka</div>
+            </div>
+            <div>
+                <div>Люблю играть в КС</div>
+                <div>Тоже играет в КС</div>
+                <div>Работает программистом</div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import s from './Navbar.module.css'
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 
 
 const Navbar = () => {
     return (
 
         <div className={s.navbar}>
-            <div><Link to="/content">Content</Link></div>
-            <div><Link to="/dialogs">Dialogs</Link></div>
+            <div><NavLink to="/content" activeClassName={s.active}>Content</NavLink></div>
+            <div><NavLink to="/dialogs" activeClassName={s.active}>Dialogs</NavLink></div>
             <div><Link to="/messages">Messages</Link></div>
             <div><a href="/news">News</a></div>
             <div><a href="/music">Music</a></div>
