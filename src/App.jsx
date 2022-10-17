eca1de8ff03d18f2cc0c65f52ea7da6fdcdfda1a
motooven <motooven@gmail.com>
2022-10-17 20:26:02 +0300

    v19

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,31 @@ import "./App.css"
 import Headers from "./components/Header/Headers";
 import Navbar from "./components/Navbar/Navbar";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import Content from "./components/Content/Content";
+import Messages from "./components/Navbar/Messages/Messages";
+import News from "./components/Navbar/News/Messages";
+import Music from "./components/Navbar/Music/Music";
+import Settings from "./components/Navbar/Settings/Messages";
 
 
 const App = () => {
     return (
-        <div className='div1'>
-            <Headers/>
-            <Navbar/>
-            <div className="content_wrapper">
-                <Dialogs/>
-                {/*<Content/>*/}
-            </div>
+        <BrowserRouter>
+            <div className='div1'>
+                <Headers/>
+                <Navbar/>
+                <div className="content_wrapper">
+                    <Route path='/content' render={Content}/>
+                    <Route path='/dialogs' render={Dialogs}/>
+                    <Route path='/messages' render={Messages}/>
+                    <Route path='/news' render={News}/>
+                    <Route path='/music' render={Music}/>
+                    <Route path='/settings' render={Settings}/>
+                </div>
 
-        </div>
+            </div>
+        </BrowserRouter>
     )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import s from './Navbar.module.css'
+import {Link} from "react-router-dom";
 
 
 const Navbar = () => {
     return (
+
         <div className={s.navbar}>
-            <div><a>Content</a></div>
-            <div><a>Messages</a></div>
-            <div><a>News</a></div>
-            <div><a>Music</a></div>
-            <div><a>Settings</a></div>
+            <div><Link to="/content">Content</Link></div>
+            <div><Link to="/dialogs">Dialogs</Link></div>
+            <div><Link to="/messages">Messages</Link></div>
+            <div><a href="/news">News</a></div>
+            <div><a href="/music">Music</a></div>
+            <div><a href="/settings">Settings</a></div>
         </div>
     )
 }
