4bca865618ee86b061f9c514890476b442d48f02
motooven <motooven@gmail.com>
2022-10-17 23:40:58 +0300

    v23

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 
 const Dialogs = (props) => {
+
+    const DialogItems = (props) => {
+        return  <div><NavLink to={"/dialogs/" + props.id} >{props.name}</NavLink></div>
+    }
+
+    const MessageItems = (props) => {
+        return <div>{props.message}</div>
+    }
+
     return (
         <div className={s.dialogs}>
             <div>
-                <div>Oleg</div>
-                <div>Kaban</div>
-                <div>Vana</div>
-                <div>Jeka</div>
-                <div>Viktor</div>
-                <div>Mihka</div>
+                <DialogItems name="Oleg" id="1" />
+                <DialogItems name="Kaban" id="2" />
+                <DialogItems name="Ivan" id="3" />
             </div>
             <div>
-                <div>Люблю играть в КС</div>
-                <div>Тоже играет в КС</div>
-                <div>Работает программистом</div>
+                <MessageItems message="Люблю играть в КС" />
+                <MessageItems message="Тоже играет в КС" />
+                <MessageItems message="Работает программистом" />
             </div>
         </div>
     )
